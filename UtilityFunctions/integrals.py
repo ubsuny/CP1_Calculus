a835a393057cb40cfa8198dd93281ede0cb76c57
shopnochari <sayeedah@buffalo.edu>
2021-11-22 13:28:35 -0500

    My solution for homework 8 part 1

--- UtilityFunctions/integrals.py
+++ UtilityFunctions/integrals.py
@@ -15,12 +15,11 @@ def simpson(f, a, b, n):
     """
     h = (b - a) / n
     i = np.arange(0,n)
-    
     s = f(a) + f(b) 
     s += 4 * np.sum( f( a + i[1::2] * h ) )
     s += 2 * np.sum( f( a + i[2:-1:2] * h ) )
-    
-    return s * h / 3
+    sum = s * h / 3
+    return sum
 
 def trapezoid(f, a, b, n):
     """Approximates the definite integral of f from a to b by
